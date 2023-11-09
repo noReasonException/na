f45c9e5fa94f9dec25ea5d1bfd6a4d58f7944e3b
Stefanos Stefanou <stefanos.stefanou@century.tech>
2023-11-09 12:24:28 +0000

    typo

--- src/test/scala/BasicSpec.scala
+++ src/test/scala/BasicSpec.scala
@@ -1,5 +1,5 @@
 
-class ValidatedNelInstancesSpec extends org.scalatest.funsuite.AnyFunSuite {
+class BasicSpec extends org.scalatest.funsuite.AnyFunSuite {
   test("Basic Example Case") {
     assert (Main.run("data_tiny.txt")==18,"data_tiny fails")
   }
