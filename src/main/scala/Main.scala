f9a05ef07b75e8175c01ee5aa8be4bbc7f60a465
Stefanos Stefanou <stefanos.stefanou@century.tech>
2023-11-09 09:20:49 +0000

    Construct with fold

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,11 @@
 object Main {
   def main(args: Array[String]): Unit = {
     val data = new DataParser("data_tiny.txt")
-    val graph = new TriangleGraph(data)
-
-    println(tree.nodes)
+    val levels = data.getLines.length
+    val nodes = (1+levels)*(levels/2)
+    val graph = TriangleGraph.build(data.getLines)
+    println(data.getLines)
+    println(graph)
   }
 
-
-  def addLevel()
 }
--- src/main/scala/TriangleGraph.scala
+++ src/main/scala/TriangleGraph.scala
@@ -1,4 +1,22 @@
 import scala.annotation.tailrec
-case class TriangleGraph (nodes:List[List[Int]]){
+import scala.util.Try
+case class TriangleGraph private (connections:List[(Int,Int)])
 
+
+
+
+object TriangleGraph{
+  def build (levels: List[List[Int]]): TriangleGraph ={
+    val connections = levels.tail.foldLeft((levels.head,List.empty[(Int,Int)]))((previousLevelWithConnections,currLevel)=>{
+      val currentConnections: List[(Int, Int)] = previousLevelWithConnections._1.zipWithIndex.flatMap(nodeWithIndex=>{
+        val left = Try.apply(currLevel(nodeWithIndex._2)).toOption
+        val right = Try.apply(currLevel(nodeWithIndex._2+1)).toOption
+        left.toList.map((nodeWithIndex._1,_))::: right.toList.map((nodeWithIndex._1,_))
+      })
+      (currLevel,previousLevelWithConnections._2:::currentConnections)
+    })
+
+    TriangleGraph(connections._2)
+
+  }
 }
